f2a951cdd57f692b20da74a554fd60298f1b840c
Pedro De Campos Faria <pedroafonso939@yahoo.com.br>
2022-05-15 13:11:31 -0300

    :D

---
+++ app.js
@@ -0,0 +1,77 @@
+
+// declara um conjunto inicial de contatos
+var db_contatos_inicial = {
+    "data": [
+        {
+            "id": 1,
+            "nome": "Paulo",
+            "cpf": "11111111111",
+        },
+        {
+            "id": 2,
+            "nome": "Pedro Afonso",
+            "cpf": "11111111111",
+           
+        },
+        
+    ]
+}
+
+
+
+// Caso os dados já estejam no Local Storage, caso contrário, carrega os dados iniciais
+var db = JSON.parse(localStorage.getItem('db_contato'));
+if (!db) {
+    db = db_contatos_inicial
+};
+
+// Exibe mensagem em um elemento de ID msg
+function displayMessage(msg) {
+    $('#msg').html('<div class="alert alert-warning">' + msg + '</div>');
+}
+
+function insertContato(contato) {
+    // Calcula novo Id a partir do último código existente no array (PODE GERAR ERRO SE A BASE ESTIVER VAZIA)
+    let novoId = 1;
+    if (db.data.length != 0) 
+      novoId = db.data[db.data.length - 1].id + 1;
+    let novoContato = {
+        "id": novoId,
+        "nome": contato.nome, 
+        "cpf": contato.cpf
+        
+    };
+
+    // Insere o novo objeto no array
+    db.data.push(novoContato);
+    displayMessage("Contato inserido com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_contato', JSON.stringify(db));
+}
+
+function updateContato(id, contato) {
+    // Localiza o indice do objeto a ser alterado no array a partir do seu ID
+    let index = db.data.map(obj => obj.id).indexOf(id);
+
+    // Altera os dados do objeto no array
+    db.data[index].nome = contato.nome,
+   
+    db.data[index].cpf = contato.cpf,
+    
+
+    displayMessage("Contato alterado com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_contato', JSON.stringify(db));
+}
+
+function deleteContato(id) {    
+    // Filtra o array removendo o elemento com o id passado
+    db.data = db.data.filter(function (element) { return element.id != id });
+
+    displayMessage("Contato removido com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_contato', JSON.stringify(db));
+}
